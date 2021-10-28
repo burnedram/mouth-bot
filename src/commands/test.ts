f78acc01e7afd58da475e7d1691d60a8bb3af82d
Rafael Mohlin <raffe_mohlin@hotmail.com>
2021-10-28 23:03:13 +0200

    Remove old test command

--- src/commands/test.ts
+++
@@ -1,9 +0,0 @@
-import { Discord, SimpleCommand, SimpleCommandMessage } from 'discordx';
-
-@Discord()
-class TestCommand {
-  @SimpleCommand('test')
-  test(command: SimpleCommandMessage) {
-    command.message.reply(`ät bajs ${command.message.member?.displayName}`);
-  }
-}
